49ae8c74ff817c4d508fe6b93081665fedea187b
Paulo Junior <paulojuniorr@tutanota.com>
2022-09-02 17:07:23 -0300

    ex012.js Bom dia

---
+++ aula12/ex012.js
@@ -0,0 +1,14 @@
+// Dando bom dia, boa tarde, boa noite
+var agora = new Date() //puxa hora do sistema
+var hora = agora.getHours() 
+console.log(`Agora são exatamente ${hora} horas.`)
+
+if (hora < 12) {
+    console.log('Bom dia!')
+} else if (hora <= 18) {
+    console.log('Boa Tarde!') 
+} else if(hora <= 24) {
+    console.log('Boa Noite')
+} else {
+    console.log('Boa Madrugada')
+}
