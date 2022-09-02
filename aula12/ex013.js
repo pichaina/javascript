5690628e26b974959f4966fbf201a34d9d232496
Paulo Junior <paulojuniorr@tutanota.com>
2022-09-02 17:07:51 -0300

    ex013.js dias da semana

---
+++ aula12/ex013.js
@@ -0,0 +1,39 @@
+// Dia da Semana
+
+/*
+    Para o JS
+    0 = Domingo
+    1 = Segunda
+    2 = Terça
+    3 = Quarta
+    4 = Quinta
+    5 = Sexta
+    6 = Sábado
+*/
+var agora = new Date() // hora/data do sistema
+var diaSem = agora.getDay() 
+//console.log(diaSem) 
+
+switch(diaSem) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break    
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+}
