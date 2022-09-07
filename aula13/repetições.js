ee0b647c1ee72076aab80d02b79807ee80140003
Paulo Junior <paulojuniorr@tutanota.com>
2022-09-07 12:41:32 -0300

    aula 13/while-do

---
+++ aula13/repetições.js
@@ -0,0 +1,24 @@
+// function do
+var c = 1 // c = contador
+do {
+    console.log(`Passo ${c}`) 
+    c++ // c = c + 1
+} while (c <= 6) 
+
+
+/* function while
+var c = 1 // c = contador
+while (c <= 6) {
+    console.log(`Passo ${c}`) 
+    c++ // c = c + 1
+}
+*/
+
+/* function
+console.log('Passo 1')
+console.log('Passo 2') 
+console.log('Passo 3') 
+console.log('Passo 4') 
+console.log('Passo 5') 
+console.log('Passo 6') 
+*/
