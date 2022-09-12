cff51b146f3546cb4bf7477c4e7f41af8cc07108
Paulo Junior <paulojuniorr@tutanota.com>
2022-09-12 13:55:47 -0300

    função001 Soma

---
+++ aula16/funcao001.js
@@ -0,0 +1,9 @@
+function parimpar(n) {
+    if (n % 2 == 0) { // % dividido por 2
+        return `Par`
+    } else {
+        return `Ímpar`
+    }     
+} 
+    console.log(parimpar(223))
+// 223 vai pro n do function o n é dividido por 2 se der resto é impar
